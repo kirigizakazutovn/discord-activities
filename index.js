79eb042e757317266917b9f50b3cfc99a6c38929
kirigizakazutovn <83147867+kirigizakazutovn@users.noreply.github.com>
2022-01-05 11:04:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Client = require("./src/Client");
 const client = new Client({
   partials: ["MESSAGE", "REACTION"],
   presence: {
-    status: "invisible",
+    status: "idle",
   },
   intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"],
 });
